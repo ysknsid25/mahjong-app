fc128eb284d31d2729958a16f450be7d3ee06e50
RamenTabetarou <kobeengineer1225@gmail.com>
2021-06-03 07:09:09 +0900

    共通ツイートのメッセージを修正
    Signed-off-by: RamenTabetarou <kobeengineer1225@gmail.com>

--- src/constants/links.js
+++ src/constants/links.js
@@ -4,7 +4,7 @@ export const googleFormUrl = "https://forms.gle/516aKVUFosamNFFb6";
 const getTweetUrl = () => {
     const twitterUtl = "https://twitter.com/intent/tweet?";
     const url = encodeURIComponent(location.href);
-    const tweet = "テストtweetだお";
+    const tweet = "";
     const hashTags = "Mahjanager";
     return (
         twitterUtl + "text=" + tweet + "&hashtags=" + hashTags + "&url=" + url
